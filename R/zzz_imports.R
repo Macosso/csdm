55d4ee8cf6f6d8651eea7d23acc3e659efaf7108
João Cláudio Macosso <63609513+Macosso@users.noreply.github.com>
2026-09-14 09:16:18 +0200

    fix: import model generics for isolated namespace loading

--- R/zzz_imports.R
+++ R/zzz_imports.R
@@ -1,3 +1,3 @@
-#' @importFrom stats aggregate cor model.matrix.lm pt residuals sd setNames symnum terms
+#' @importFrom stats aggregate coef cor df.residual fitted formula model.frame model.matrix model.matrix.lm nobs predict pt residuals sd setNames symnum terms update vcov
 #' @importFrom Rdpack reprompt
 NULL
